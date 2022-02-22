48671ee56e4ce5dfdfe19a5fa29a2ee92f982e48
Kranelzin <62190395+Kranelzin@users.noreply.github.com>
2022-02-22 09:46:48 -0300

    Adicionando python pra ela não chora!
    Correção de bugs bugados e de path dos arquivos.

---
+++ bot/botv2.py
@@ -0,0 +1,90 @@
+from __future__ import print_function, unicode_literals
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+import funcoes
+import os
+ 
+ 
+def main(args):
+    print(__file__)
+    print(os.path.abspath(os.path.dirname(__file__)))
+ 
+    return 0
+ 
+
+
+numero_de_linhas = 0
+
+#ABRIR ARQUIVO DE DESTINATARIOS TXT
+arquivoc = open(os.path.abspath(os.path.dirname(__file__))+"/pessoas.txt",'rb')
+contatos = []
+for line in arquivoc:
+    escrever_contatos = line.rstrip().decode('utf-8')
+    contatos.append(escrever_contatos)
+    numero_de_linhas = numero_de_linhas + 1
+arquivoc.close()    
+
+class Botezinhomelhor:
+    def __init__(self):
+
+        self.digitar_pessoas_aqui= contatos
+        options = webdriver.ChromeOptions()
+        options.add_argument('lang=pt-br')
+        self.driver = webdriver.Chrome(executable_path= os.path.abspath(os.path.dirname(__file__))+"/chromedriver.exe", chrome_options=options)
+    
+    
+    def encaminhar(self):
+
+        self.driver.get('https://web.whatsapp.com')
+        time.sleep(5)
+        self.driver.save_screenshot("../projetobot/public/img/qrcode.png")
+        envio_para_contatos = 0
+        contagem = 0
+        contagemlinhas = 0
+
+        for pessoas in contatos:
+
+            contagemlinhas = contagemlinhas + 1
+
+            if envio_para_contatos == 0: 
+                funcoes.enviofuncoes.tentarenvio(self)
+            
+            
+            pesquisar_contatos = self.driver.find_element_by_xpath('//*[@id="app"]/div/span[2]/div/span/div/div/div/div/div/div/div[1]/div/label/div/div[2]')
+            time.sleep(2)
+            pesquisar_contatos.click()
+            pesquisar_contatos.send_keys(pessoas)
+            time.sleep(1)
+            contagem = contagem + funcoes.enviofuncoes.tentarcontatos(pessoas)
+            
+            envio_para_contatos = 1
+
+                
+            for i in range(len(pessoas)):
+                pesquisar_contatos.send_keys(Keys.BACKSPACE)
+
+
+            if contagem == 5 or contagemlinhas == numero_de_linhas:
+                funcoes.enviofuncoes.enviarmensagem(self)
+                envio_para_contatos = 0
+                contagem = 0
+                
+        log_de_erro = open("LogErro.txt",'a')
+        data_e_hora_atuais = datetime.now()
+        data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+        log_de_erro.write("Log de Erro gerado em: ")
+        log_de_erro.write(data_e_hora_em_texto)
+        log_de_erro.write("\n")
+        log_de_erro.close()
+        
+ 
+
+
+teste = Botezinhomelhor()
+teste.encaminhar()
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
